fdc8fba859357497f76f1abcd502a4fa2b37ab52
ReginaDiloyy <diloyregina@gmail.com>
2021-07-13 14:46:17 +0800

    Add files via upload

---
+++ AddProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace G3D
+{
+
+    public partial class AddProduct : Form
+    {
+        public AddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login ss = new Login();
+            ss.Show();
+        }
+
+        private void AddProduct_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
